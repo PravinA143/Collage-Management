ff79d9dea3bcc332f99e7085fd03cf2fed457d14
PRAVIN LAXMAN HELALAR <pravinhelkar4@gmail.com>
2025-01-31 19:54:04 +0530

    File Upload

---
+++ StudentLogin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Demo</title>
+    
+    <link rel="stylesheet" href="StudentLogin.css">
+
+
+</head>
+<body>
+
+	<div class="abc">
+		
+    <div class="navbar">
+
+<div class="logo"><img src="logo.png" alt="pravin";> </div>
+
+<div class="clg-name"><p>Shri Shivaji Mahavidyalaya,Paranda</p><p class="next-line">Student Login Page</p></div>
+
+<!-- <div class="content-main">	
+
+<ul type="none" class="content">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="StudentLogin.php">Students</a></li>
+        <li><a href="#">Collages</a></li>
+        <li><a href="#">Contact</a></li>
+
+</ul>
+
+</div> -->
+
+</div>	
+		
+
+		<div class="login-container">
+			
+			<div class="login"><b>Sign in </b></div>
+
+			<form class="enter">
+				
+				<input type="text" placeholder="Enter Email Here" required>
+				<input type="password" placeholder="Enter Password Here" required>
+				<button>Login</button>
+				
+				<p>Dont Have An Account?</p>
+				<a href="#">Sign Up</a><span>Here </span>
+
+			</form>
+		
+		</div>
+
+	</div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>project</title>
+
+   <link rel="stylesheet"  type="text/css" href="index.css">
+    <!-- <link rel="stylesheet"  type="text/css" href="login.php"> -->
+
+
+</head>
+<body>
+
+	<div class="main-div">
+		
+		<div class="navbar">
+
+			<div class="logo"><img src="logo.png" alt="pravin";> </div>
+
+			<div class="clg-name"><p class="title">Shri Shivaji Mahavidyalaya,Paranda</p></div>
+
+		    <div class="content-main">	
+	    
+		<ul type="none" class="content">
+		            <li><a href="index.php">Home</a></li>
+                    <li><a href="StudentLogin.php">Students Login</a></li>
+                    <li><a href="#">Collage Login</a></li>
+					<li><a href="#">Contact</a></li>
+
+		</ul>
+
+			</div>
+
+		</div>
+
+		<div class="welcome"><center><b>Welcome to  ExamOnline ! Online Portal of Examination Section, Shri Shivaji Mahavidyalaya, Paranda.</b></center></div>
+
+		<!-- <div class="prof-info"><div><img src="image.jpg"></div><b>Prof.(Dr.) T.N.Lokhande</b> -->
+
+		 </div>
+
+	</div>
+
+
+</body>
+</html>
+
+<li><a href="../index.html">Home</a></li>
